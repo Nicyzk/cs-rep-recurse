b4620e8a60b99b9bad9b15a2b96f29c28910f0d2
Sui Kiat Lim <suikiat00@gmail.com>
2022-09-18 04:08:36 +0800

    changed stack code

--- codeitsuisse/routes/stack_pwn_runner.py
+++ codeitsuisse/routes/stack_pwn_runner.py
@@ -11,10 +11,10 @@ logger = logging.getLogger(__name__)
 
 @app.route('/payload_stack', methods=['GET'])
 def payload_stack():
-    io = pwn.process('./stack')
+    # io = pwn.process('./stack')
     with open('./payload_stack', 'rb') as f:
         payload = f.read()
 
-    io.send(payload)
+    # io.send(payload)
     # output = io.recv().decode('latin-1')
     return json.dumps(payload)
