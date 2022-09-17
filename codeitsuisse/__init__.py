eab188d4bb8c69e97d88468259f55c8bcdca6352
Sui Kiat Lim <suikiat00@gmail.com>
2022-09-17 16:25:38 +0800

    added ticker streams and crypto collapz to init

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.ticker_stream_part1
+import codeitsuisse.routes.ticker_stream_part2
+import codeitsuisse.routes.crypto_collapz
 
 
 
